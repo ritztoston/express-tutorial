f879622f549c6138a3ca2f7ed2fdcd103c125a3f
ritztoston <ritztoston@gmail.com>
2018-06-10 19:24:22 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.use((req, res, next) => {
+
+});
+
+
+app.get('/', (req, res) => {
+    /*res.send('<h1>Hello Express</h1>');
+    res.send({
+        name: 'Kim',
+        likes: [
+            'Bohol',
+            'Cebu'
+        ]
+    });*/
+    res.render('home.hbs', {
+        name: 'Kim',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Err: Unable to handle request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up!');
+});
